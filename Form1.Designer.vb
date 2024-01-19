8fc92f44b72b68756d5c353cfa3d13a9274197f5
Lenovo-User <Lenovo-User@DESKTOP-JS25F1I>
2024-01-19 15:51:22 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtWord = New TextBox()
+        WordListBox = New ListBox()
+        btnAdd = New Button()
+        DisplayLabel = New Label()
+        ToUpperRadioButton = New RadioButton()
+        ToLowerRadioButton = New RadioButton()
+        PurpleRadioButton = New RadioButton()
+        BlueRadioButton = New RadioButton()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(292, 23)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(93, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter a word"
+        ' 
+        ' txtWord
+        ' 
+        txtWord.Location = New Point(272, 57)
+        txtWord.Name = "txtWord"
+        txtWord.Size = New Size(187, 27)
+        txtWord.TabIndex = 1
+        ' 
+        ' WordListBox
+        ' 
+        WordListBox.FormattingEnabled = True
+        WordListBox.Location = New Point(571, 57)
+        WordListBox.Name = "WordListBox"
+        WordListBox.Size = New Size(150, 104)
+        WordListBox.TabIndex = 2
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Font = New Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnAdd.Location = New Point(272, 124)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(113, 48)
+        btnAdd.TabIndex = 3
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' DisplayLabel
+        ' 
+        DisplayLabel.AutoSize = True
+        DisplayLabel.Location = New Point(310, 316)
+        DisplayLabel.Name = "DisplayLabel"
+        DisplayLabel.Size = New Size(40, 20)
+        DisplayLabel.TabIndex = 4
+        DisplayLabel.Text = "Case"
+        ' 
+        ' ToUpperRadioButton
+        ' 
+        ToUpperRadioButton.AutoSize = True
+        ToUpperRadioButton.Location = New Point(310, 354)
+        ToUpperRadioButton.Name = "ToUpperRadioButton"
+        ToUpperRadioButton.Size = New Size(91, 24)
+        ToUpperRadioButton.TabIndex = 5
+        ToUpperRadioButton.TabStop = True
+        ToUpperRadioButton.Text = "To Upper"
+        ToUpperRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' ToLowerRadioButton
+        ' 
+        ToLowerRadioButton.AutoSize = True
+        ToLowerRadioButton.Location = New Point(310, 400)
+        ToLowerRadioButton.Name = "ToLowerRadioButton"
+        ToLowerRadioButton.Size = New Size(90, 24)
+        ToLowerRadioButton.TabIndex = 6
+        ToLowerRadioButton.TabStop = True
+        ToLowerRadioButton.Text = "To Lower"
+        ToLowerRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' PurpleRadioButton
+        ' 
+        PurpleRadioButton.AutoSize = True
+        PurpleRadioButton.Location = New Point(600, 354)
+        PurpleRadioButton.Name = "PurpleRadioButton"
+        PurpleRadioButton.Size = New Size(72, 24)
+        PurpleRadioButton.TabIndex = 7
+        PurpleRadioButton.TabStop = True
+        PurpleRadioButton.Text = "Purple"
+        PurpleRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' BlueRadioButton
+        ' 
+        BlueRadioButton.AutoSize = True
+        BlueRadioButton.Location = New Point(600, 400)
+        BlueRadioButton.Name = "BlueRadioButton"
+        BlueRadioButton.Size = New Size(59, 24)
+        BlueRadioButton.TabIndex = 8
+        BlueRadioButton.TabStop = True
+        BlueRadioButton.Text = "Blue"
+        BlueRadioButton.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(617, 316)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(53, 20)
+        Label2.TabIndex = 9
+        Label2.Text = "Colour"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label2)
+        Controls.Add(BlueRadioButton)
+        Controls.Add(PurpleRadioButton)
+        Controls.Add(ToLowerRadioButton)
+        Controls.Add(ToUpperRadioButton)
+        Controls.Add(DisplayLabel)
+        Controls.Add(btnAdd)
+        Controls.Add(WordListBox)
+        Controls.Add(txtWord)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtWord As TextBox
+    Friend WithEvents WordListBox As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents ToUpperRadioButton As RadioButton
+    Friend WithEvents ToLowerRadioButton As RadioButton
+    Friend WithEvents PurpleRadioButton As RadioButton
+    Friend WithEvents BlueRadioButton As RadioButton
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿
+Public Class Form1
+    Private wordList As New List(Of String)
+
+    Private Sub btnAddButton_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim word As String = txtWord.Text.Trim()
+
+        If Not String.IsNullOrEmpty(word) AndAlso Not wordList.Contains(word) Then
+            wordList.Add(word)
+            RefreshWordList()
+        Else
+            MsgBox("word already exist, please enter new word.")
+        End If
+    End Sub
+
+    Private Sub RefreshWordList()
+        WordListBox.Items.Clear()
+        WordListBox.Items.AddRange(wordList.ToArray())
+    End Sub
+
+    Private Sub WordListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles WordListBox.SelectedIndexChanged
+        If WordListBox.SelectedIndex <> -1 Then
+            Dim selectedWord As String = WordListBox.SelectedItem.ToString()
+            DisplayLabel.Text = selectedWord
+            DisplayLabel.BackColor = Color.White
+        End If
+    End Sub
+
+    Private Sub RadioButton_CheckedChanged(sender As Object, e As EventArgs) Handles ToUpperRadioButton.CheckedChanged,
+                                                                                   ToLowerRadioButton.CheckedChanged,
+                                                                                   PurpleRadioButton.CheckedChanged,
+                                                                                   BlueRadioButton.CheckedChanged
+
+        If WordListBox.SelectedIndex <> -1 Then
+            Dim selectedWord As String = WordListBox.SelectedItem.ToString()
+
+            If ToUpperRadioButton.Checked Then
+                DisplayLabel.Text = selectedWord.ToUpper()
+            ElseIf ToLowerRadioButton.Checked Then
+                DisplayLabel.Text = selectedWord.ToLower()
+            ElseIf PurpleRadioButton.Checked Then
+                DisplayLabel.ForeColor = Color.Purple
+            ElseIf BlueRadioButton.Checked Then
+                DisplayLabel.ForeColor = Color.Blue
+            End If
+        End If
+    End Sub
+End Class
